c0fabe99ad02b02b003a44311e0032703b118860
Trinity-142 <n.sharapov.nsk@gmail.com>
2025-09-05 19:24:24 +0700

    Fix heap, new tests, script

--- Task_1_1/src/main/java/ru/nsu/sharapov/HeapSort.java
+++ Task_1_1/src/main/java/ru/nsu/sharapov/HeapSort.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class Heap {
-    ArrayList<Integer> heap = new ArrayList<>();
+    ArrayList<Integer> heap;
 
-    public void insert(int value) {
-        heap.add(value);
-        _siftup(heap.size() - 1);
+    public Heap(ArrayList<Integer> array) {
+        heap = array;
+        for (int i = array.size() / 2 - 1; i >= 0; --i) {
+            _siftdown(i);
+        }
     }
 
-    public int extract_max() {
+    public int extract_min() {
         int max = heap.getFirst();
         Collections.swap(heap, 0, heap.size() - 1);
         heap.removeLast();
@@ -20,26 +22,14 @@ class Heap {
 
     }
 
-    public void _siftup(int pos) {
-        while (pos > 0) {
-            int parent = (pos - 1) / 2;
-            if (heap.get(parent) > heap.get(pos)) {
-                Collections.swap(heap, parent, pos);
-                pos = parent;
-            } else break;
-        }
-    }
-
-
     public void _siftdown(int pos) {
         while (true) {
             int curr = pos;
             int left = 2 * pos + 1;
             int right = 2 * pos + 2;
-            if (left < heap.size() && heap.get(left) < heap.get(curr))
-                curr = left;
-            if (right < heap.size() && heap.get(right) < heap.get(curr))
-                curr = left;
+            if (left < heap.size() && heap.get(left) < heap.get(curr)) curr = left;
+            if (right < heap.size() && heap.get(right) < heap.get(curr) && heap.get(right) < heap.get(left))
+                curr = right;
             if (curr != pos) {
                 Collections.swap(heap, pos, curr);
                 pos = curr;
@@ -50,16 +40,25 @@ class Heap {
 
 
 public class HeapSort {
-    public static Integer[] sort(Integer[] args) {
-        Heap myheap = new Heap();
-        for (int i : args) {
-            myheap.insert(i);
-        }
+    public static Integer[] sort(ArrayList<Integer> args) {
+        Heap myheap = new Heap(args);
         ArrayList<Integer> actual = new ArrayList<>();
         while (!myheap.heap.isEmpty()) {
-            actual.add(myheap.extract_max());
+            actual.add(myheap.extract_min());
         }
 
         return actual.toArray(new Integer[0]);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (String i : args) {
+            integers.add(Integer.parseInt(i));
+        }
+        Integer[] res = sort(integers);
+        for (int i : res) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
     }
 }
--- Task_1_1/src/test/java/ru/nsu/sharapov/Test.java
+++ Task_1_1/src/test/java/ru/nsu/sharapov/Test.java
@@ -2,15 +2,34 @@ package ru.nsu.sharapov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SampleTest {
 
     @Test
-    void checkSort() {
-        Integer[] test = {5, 4, 3, 2, 1};
+    void checkSort1() {
+        ArrayList<Integer> test = new ArrayList<>(Arrays.asList(10, 3, 15, 4, 2));
         Integer[] actual = HeapSort.sort(test);
-        Integer[] expected = {1, 2, 3, 4, 5};
+        Integer[] expected = {2, 3, 4, 10, 15};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void checkSort2() {
+        ArrayList<Integer> test = new ArrayList<>(Arrays.asList(9, 1, 20, 6, 9, 13));
+        Integer[] actual = HeapSort.sort(test);
+        Integer[] expected = {1, 6, 9, 9, 13, 20};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void checkSort3() {
+        ArrayList<Integer> test = new ArrayList<>(Arrays.asList(5, 4, 3, 2, 1, 0));
+        Integer[] actual = HeapSort.sort(test);
+        Integer[] expected = {0, 1, 2, 3, 4, 5};
         assertArrayEquals(actual, expected);
     }
 }
