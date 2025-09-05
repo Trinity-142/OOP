823f0d3dcbc679a91739b840a900ac4ad35d14d1
Trinity-142 <n.sharapov.nsk@gmail.com>
2025-09-05 20:27:01 +0700

    Replace getFirst and removeLast

--- Task_1_1/src/main/java/ru/nsu/sharapov/HeapSort.java
+++ Task_1_1/src/main/java/ru/nsu/sharapov/HeapSort.java
@@ -4,11 +4,50 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 
+/**
+ * Класс сортировки
+ */
+public class HeapSort {
+    /**
+     * Сортирует массив целых чисел по возрастанию с использованием пирамидальной сортировки.
+     *
+     * @param args Элементы неостортированного массива
+     * @return Возвращает отсортированный массив
+     */
+    public static Integer[] sort(ArrayList<Integer> args) {
+        Heap myheap = new Heap(args);
+        ArrayList<Integer> actual = new ArrayList<>();
+        while (!myheap.heap.isEmpty()) {
+            actual.add(myheap.extract_min());
+        }
+
+        return actual.toArray(new Integer[0]);
+    }
+
+    /**
+     * Сортирует массив и выводит результат.
+     *
+     * @param args Строковый массив чисел
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (String i : args) {
+            integers.add(Integer.parseInt(i));
+        }
+        Integer[] res = sort(integers);
+        for (int i : res) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+    }
+}
+
 class Heap {
     ArrayList<Integer> heap;
 
     /**
-     * Конструктор класса, преобразует массив в пирамиду
+     * Конструктор класса, преобразует массив в пирамиду.
+     *
      * @param array Любой входной массив целых чисел, который будет преобразован в неубывающую пирамиду
      */
     public Heap(ArrayList<Integer> array) {
@@ -19,19 +58,21 @@ class Heap {
     }
 
     /**
-     * Извлекает минимальный элемент кучи
+     * Извлекает минимальный элемент кучи.
+     *
      * @return Целое число, минимальный элемент кучи
      */
     public int extract_min() {
-        int min = heap.getFirst();
+        int min = heap.get(0);
         Collections.swap(heap, 0, heap.size() - 1);
-        heap.removeLast();
+        heap.remove(heap.size() - 1);
         _siftdown(0);
         return min;
     }
 
     /**
-     * "Просеивание вниз" - просеивает элемент по заданному индексу до своей корректной позиции
+     * "Просеивание вниз" - просеивает элемент по заданному индексу до своей корректной позиции.
+     *
      * @param pos Индекс элемента, который необходимо "просеить" вниз по пирамиде
      */
     public void _siftdown(int pos) {
@@ -47,41 +88,5 @@ class Heap {
                 pos = curr;
             } else break;
         }
-    }
-}
-
-/**
- * Класс сортировки
- */
-public class HeapSort {
-    /**
-     * Сортирует массив целых чисел по возрастанию с использованием пирамидальной сортировки
-     * @param args Элементы неостортированного массива
-     * @return Возвращает отсортированный массив
-     */
-    public static Integer[] sort(ArrayList<Integer> args) {
-        Heap myheap = new Heap(args);
-        ArrayList<Integer> actual = new ArrayList<>();
-        while (!myheap.heap.isEmpty()) {
-            actual.add(myheap.extract_min());
-        }
-
-        return actual.toArray(new Integer[0]);
-    }
-
-    /**
-     * Сортирует массив и выводит результат
-     * @param args Строковый массив чисел
-     */
-    public static void main(String[] args) {
-        ArrayList<Integer> integers = new ArrayList<>();
-        for (String i : args) {
-            integers.add(Integer.parseInt(i));
-        }
-        Integer[] res = sort(integers);
-        for (int i : res) {
-            System.out.printf("%d ", i);
-        }
-        System.out.println();
     }
 }
